ea29b7fc503dbf3dde33b7619be96c02b9cfc7ae
JohanPerera <johanpriyasanka@gmail.com>
2020-10-23 13:42:32 +0530

    Initialized File Architecture

---
+++ src/app/boards/itinerary-map/itinerary-map.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-itinerary-map',
+  templateUrl: './itinerary-map.component.html',
+  styleUrls: ['./itinerary-map.component.css']
+})
+export class ItineraryMapComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
