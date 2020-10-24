d65044f313288fd050ae32e090dceab57d30368e
JohanPerera <johanpriyasanka@gmail.com>
2020-10-24 18:20:47 +0530

    Map Component Done

--- src/app/shared/map/map.component.ts
+++ src/app/shared/map/map.component.ts
@@ -1,15 +1,17 @@
 import { Component, OnInit } from '@angular/core';
 
+
 @Component({
   selector: 'app-map',
   templateUrl: './map.component.html',
   styleUrls: ['./map.component.css']
 })
 export class MapComponent implements OnInit {
-
+  lat = 6.928309;
+  lng = 79.887617;
   constructor() { }
 
   ngOnInit(): void {
   }
-
+//, 
 }
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,16 +2,24 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MapComponent } from './map/map.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
-
+import {FlexLayoutModule} from '@angular/flex-layout'
+import {AgmCoreModule}from '@agm/core';
 
 
 @NgModule({
   declarations: [MapComponent, NavBarComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    FlexLayoutModule,
+    AgmCoreModule.forRoot({
+      apiKey:'AIzaSyAnQIrbexL15BeN97xOY7nOjcQkJZfJsig'
+    })
   ],
   exports:[
     MapComponent,NavBarComponent
   ]
 })
 export class SharedModule { }
+
+
+//AIzaSyA9rPesD1LpqsLTVFeHEpxvjnl5FNLDbS0
