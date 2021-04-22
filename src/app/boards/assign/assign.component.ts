ccf11667632a3bb17ceac16d708469d5eb4a236e
Nirosh Neminda <50820417+NiroNemi@users.noreply.github.com>
2021-04-22 14:05:55 +0530

    changes

--- src/app/boards/assign/assign.component.ts
+++ src/app/boards/assign/assign.component.ts
@@ -1,7 +1,9 @@
-import { Component, OnInit } from '@angular/core';
-import { AvailabilityServiceService } from 'src/app/services/availability-service.service';
+import {Component, OnInit} from '@angular/core';
+import {AvailabilityServiceService} from 'src/app/services/availability-service.service';
 import * as moment from 'moment';
 import {HttpClient} from "@angular/common/http";
+import {AssignService} from "../../services/assign/assign.service";
+import {Grade} from "../../models/grade.model";
 
 @Component({
   selector: 'app-assign',
@@ -12,17 +14,19 @@ export class AssignComponent implements OnInit {
 
   availabilities: any;
   displayAvailabilities: any;
-
+  searchTerm: any;
+  travelAgents = [];
 
 
   constructor(
     private http: HttpClient,
     private availabilityService: AvailabilityServiceService,
+    private assignService: AssignService
 
   ) { }
 
   ngOnInit(): void {
-
+    this.loadAgents();
     this.loadAvailability()
   }
 
@@ -43,6 +47,7 @@ export class AssignComponent implements OnInit {
     this.displayAvailabilities = this.availabilities;
   }
 
+  // @ts-ignore
   onSearch(term: any) {
     console.log(moment(term).format('YYYY-MM-DD'));
     console.log(term);
@@ -54,6 +59,33 @@ export class AssignComponent implements OnInit {
        return moment(a.date).format('YYYY-MM-DD') === term
       });
     }
+
+  }
+
+  onAssign(){
+    let payload = this.displayAvailabilities.forEach((m) => {
+      let obj =  {customer: m.cust_id._id, travel_agent: m.cust_id.default_agent_id._id, iti_date: m.date};
+      this.assignService.createAssign(obj)
+        .subscribe(
+          res => {
+            console.log(res)
+          },
+          error => {
+
+          }
+        );
+    })
+
+
+  }
+
+  loadAgents(){
+    this.http.get<{ status: string, msg: string, data: Grade[] }>('http://localhost:3000/ta-agents').subscribe((postData) => {
+      this.travelAgents = postData['data'];
+      console.log(this.travelAgents);
+
+    });
+
   }
 
 
--- src/app/services/availability-service.service.ts
+++ src/app/services/availability-service.service.ts
@@ -7,17 +7,19 @@ import {HttpClient} from '@angular/common/http';
 })
 export class AvailabilityServiceService {
 
+  apiEndPoint = 'http://localhost:3000';
+
   constructor(
     private http: HttpClient
   ) { }
   createAvailability(data: any): Observable<any> {
     return this.http.post('http://localhost:3000/availability', data);
-  } 
+  }
   getAllAvailability(): Observable<any> {
     return this.http.get('http://localhost:3000/availability');
   }
 
   deleteAvailability(date: Date, custid: String) {
-    return this.http.delete('http://localhost:3000/availability/' + date + '/' + custid);
+    return this.http.delete(this.apiEndPoint+ '/availability/' + date + '/' + custid);
   }
 }
